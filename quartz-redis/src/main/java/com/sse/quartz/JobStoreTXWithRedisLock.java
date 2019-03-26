a2e328c318d46255d190e66ef03127cd8b0b3bc1
ldang264 <820704815@qq.com>
2019-03-26 13:37:10 +0800

    optimize redis lock jobStore

--- quartz-redis/src/main/java/com/sse/quartz/JobStoreTXWithRedisLock.java
+++ quartz-redis/src/main/java/com/sse/quartz/JobStoreTXWithRedisLock.java
@@ -1,6 +1,9 @@
 package com.sse.quartz;
 
-import io.lettuce.core.*;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.ScriptOutputType;
+import io.lettuce.core.SetArgs;
 import io.lettuce.core.api.StatefulRedisConnection;
 import io.lettuce.core.api.sync.RedisCommands;
 import org.quartz.JobPersistenceException;
@@ -8,6 +11,7 @@ import org.quartz.SchedulerConfigException;
 import org.quartz.impl.jdbcjobstore.JobStoreTX;
 import org.quartz.spi.ClassLoadHelper;
 import org.quartz.spi.SchedulerSignaler;
+
 import java.sql.Connection;
 import java.util.UUID;
 
@@ -20,19 +24,16 @@ public class JobStoreTXWithRedisLock extends JobStoreTX {
 
     private RedisCommands<String, String> redisCommands;
 
-    private static final String UNLOCK_LUA;
-
     /**
      * lua for release lock
      */
-    static {
-        UNLOCK_LUA = "if redis.call(\"get\",KEYS[1]) == ARGV[1] " +
-                     "then " +
-                     "    return redis.call(\"del\",KEYS[1]) " +
-                     "else " +
-                     "    return 0 " +
-                     "end ";
-    }
+    private static final String UNLOCK_LUA =
+            "if redis.call(\"get\",KEYS[1]) == ARGV[1] " +
+            "then " +
+            "    return redis.call(\"del\",KEYS[1]) " +
+            "else " +
+            "    return 0 " +
+            "end ";
 
     /**
      * host. default is localhost
@@ -60,22 +61,14 @@ public class JobStoreTXWithRedisLock extends JobStoreTX {
     protected boolean ssl;
 
     /**
-     * default expire seconds
+     * default expire milliseconds
      */
-    protected int expireInSeconds = 2;
+    protected long expireInMills = 1500;
 
     @Override
     public void initialize(ClassLoadHelper classLoadHelper, SchedulerSignaler schedSignaler) throws SchedulerConfigException {
         super.initialize(classLoadHelper, schedSignaler);
-        //init redis
-        RedisURI redisURI = RedisURI.builder().redis(host, port).withSsl(ssl).withDatabase(database).build();
-        if (password != null && !"".equals(password.trim())) {
-            redisURI.setPassword(password);
-        }
-        //RedisURI redisURI = RedisURI.create(host, port);
-        this.redisClient = RedisClient.create(redisURI);
-        StatefulRedisConnection<String, String> connect = redisClient.connect();
-        this.redisCommands = connect.sync();
+        this.initializeRedisClient();
     }
 
     @Override
@@ -86,7 +79,7 @@ public class JobStoreTXWithRedisLock extends JobStoreTX {
         try {
             if (lockName != null) {
                 lockValue = UUID.randomUUID().toString();
-                transOwner = lock(lockName, lockValue, expireInSeconds);
+                transOwner = lock(lockName, lockValue, expireInMills);
             }
 
             if (conn == null) {
@@ -136,11 +129,11 @@ public class JobStoreTXWithRedisLock extends JobStoreTX {
      * lock
      * @param key
      * @param value
-     * @param expire
+     * @param expireInMills
      * @return
      */
-    private boolean lock(String key, String value, long expire) {
-        return this.redisCommands.set(key, value, new SetArgs().ex(expire).nx()) != null;
+    private boolean lock(String key, String value, long expireInMills) {
+        return this.redisCommands.set(key, value, new SetArgs().px(expireInMills).nx()) != null;
     }
 
     /**
@@ -151,6 +144,16 @@ public class JobStoreTXWithRedisLock extends JobStoreTX {
      */
     private boolean release(String key, String value) {
         return this.redisCommands.eval(UNLOCK_LUA, ScriptOutputType.BOOLEAN, new String[]{key}, value);
+    }
+
+    private void initializeRedisClient() {
+        RedisURI redisURI = RedisURI.builder().redis(host, port).withSsl(ssl).withDatabase(database).build();
+        if (password != null && !"".equals(password.trim())) {
+            redisURI.setPassword(password);
+        }
+        this.redisClient = RedisClient.create(redisURI);
+        StatefulRedisConnection<String, String> connect = redisClient.connect();
+        this.redisCommands = connect.sync();
     }
 
     /* ---------- Note: set methods must be void----------- */
@@ -193,5 +196,13 @@ public class JobStoreTXWithRedisLock extends JobStoreTX {
 
     public void setSsl(boolean ssl) {
         this.ssl = ssl;
+    }
+
+    public long getExpireInMills() {
+        return expireInMills;
+    }
+
+    public void setExpireInMills(long expireInMills) {
+        this.expireInMills = expireInMills;
     }
 }
