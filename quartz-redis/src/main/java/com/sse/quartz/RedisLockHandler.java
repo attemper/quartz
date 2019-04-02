3b926a0bd2975ecfa96f4ce15a92e4ff6a3f9f50
ldang264 <ldang@sse.com.cn>
2019-04-02 23:12:18 +0800

    add a store extends JobStoreCMT using redis lock

---
+++ quartz-redis/src/main/java/com/sse/quartz/RedisLockHandler.java
@@ -0,0 +1,61 @@
+package com.sse.quartz;
+
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.ScriptOutputType;
+import io.lettuce.core.SetArgs;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
+
+public class RedisLockHandler {
+
+    private RedisClient redisClient;
+
+    private RedisCommands<String, String> redisCommands;
+    /**
+     * lua for release lock
+     */
+    private static final String UNLOCK_LUA =
+            "if redis.call(\"get\",KEYS[1]) == ARGV[1] " +
+                    "then " +
+                    "    return redis.call(\"del\",KEYS[1]) " +
+                    "else " +
+                    "    return 0 " +
+                    "end ";
+
+    public RedisLockHandler(String host, int port, int database, String password, boolean ssl) {
+        RedisURI redisURI = RedisURI.builder().redis(host, port).withSsl(ssl).withDatabase(database).build();
+        if (password != null && !"".equals(password.trim())) {
+            redisURI.setPassword(password);
+        }
+        this.redisClient = RedisClient.create(redisURI);
+        StatefulRedisConnection<String, String> connect = redisClient.connect();
+        this.redisCommands = connect.sync();
+    }
+    /**
+     * lock
+     * @param key
+     * @param value
+     * @param expireInMills
+     * @return
+     */
+    public boolean lock(String key, String value, long expireInMills) {
+        return this.redisCommands.set(key, value, new SetArgs().px(expireInMills).nx()) != null;
+    }
+
+    /**
+     * release lock
+     * @param key
+     * @param value
+     * @return
+     */
+    public boolean release(String key, String value) {
+        return this.redisCommands.eval(UNLOCK_LUA, ScriptOutputType.BOOLEAN, new String[]{key}, value);
+    }
+
+    public void shutdown() {
+        if (redisClient != null) {
+            redisClient.shutdown();
+        }
+    }
+}
