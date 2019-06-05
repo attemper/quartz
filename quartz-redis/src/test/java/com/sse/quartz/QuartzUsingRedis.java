7b71f51b59726f010994bb23a56341aeba819d25
ldang264 <820704815@qq.com>
2019-06-05 21:11:28 +0800

    remove some modules

--- quartz-redis/src/test/java/com/sse/quartz/QuartzUsingRedis.java
+++ quartz-redis/src/test/java/com/sse/quartz/QuartzUsingRedis.java
@@ -1,9 +1,9 @@
 package com.sse.quartz;
 
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.quartz.SchedulerFactory;
+import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
+
+import java.util.Date;
 
 /**
  * @author ldang
@@ -13,15 +13,16 @@ public class QuartzUsingRedis {
     public static void main(String[] args) throws SchedulerException {
         SchedulerFactory sf = new StdSchedulerFactory();
         Scheduler scheduler = sf.getScheduler();
-        /*//定义一个Trigger
+        //定义一个Trigger
         Trigger trigger = TriggerBuilder.newTrigger().withIdentity("trigger4", "group1") //定义name/group
-                .startNow()//一旦加入scheduler，立即生效
+                .startAt(new Date(new Date().getTime() + 10))
                 .withSchedule(SimpleScheduleBuilder.simpleSchedule() //使用SimpleTrigger
                         .withIntervalInSeconds(30) //每隔30秒执行一次
                         .repeatForever()) //一直执行，奔腾到老不停歇
                 .build();
 
-        //定义一个JobDetail
+
+        /*//定义一个JobDetail
         JobDetail job = JobBuilder.newJob(HelloJob.class) //定义Job类为HelloQuartz类，这是真正的执行逻辑所在
                 .withIdentity("job4", "group1") //定义name/group
                 .usingJobData("name", "quartz") //定义属性
