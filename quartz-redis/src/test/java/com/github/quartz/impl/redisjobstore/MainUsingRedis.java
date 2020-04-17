537836bfe6faf3b6f33b025d28d3746466aeae44
ldang264 <820704815@qq.com>
2020-04-17 18:45:24 +0800

    optimize

--- quartz-redis/src/test/java/com/github/quartz/impl/redisjobstore/MainUsingRedis.java
+++ quartz-redis/src/test/java/com/github/quartz/impl/redisjobstore/MainUsingRedis.java
@@ -9,18 +9,7 @@ import java.util.Properties;
 public class MainUsingRedis {
 
     public static void main(String[] args) throws Exception {
-        Properties properties = new Properties();
-        properties.put("org.quartz.scheduler.instanceName", "MyRedisScheduler");
-        properties.put("org.quartz.jobStore.class", "com.github.quartz.impl.redisjobstore.RedisJobStore");
-
-        properties.put("org.quartz.scheduler.instanceId", "AUTO");
-        properties.put("org.quartz.threadPool.class", "org.quartz.simpl.SimpleThreadPool");
-        properties.put("org.quartz.threadPool.threadCount", "25");
-        properties.put("org.quartz.threadPool.threadPriority", "5");
-        properties.put("org.quartz.jobStore.misfireThreshold", "60000");
-        properties.put("org.quartz.jobStore.isClustered", "true");
-        //properties.put("org.quartz.jobStore.clusterCheckinInterval", "20000");
-
+        Properties properties = Util.getRedisProperties();
         SchedulerFactory sf = new StdSchedulerFactory(properties);
         Scheduler scheduler = sf.getScheduler();
         //scheduler.clear();
